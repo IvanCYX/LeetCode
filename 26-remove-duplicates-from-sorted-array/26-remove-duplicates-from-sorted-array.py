571f1b71e72c8fd58b16bd97cd3e1c59b0ed4dae
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2022-08-05 00:16:25 -0500

    Time: 124 ms (41.25%), Space: 14.7 MB (65.15%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -4,10 +4,14 @@ class Solution(object):
         count = 0
         
         for x in nums:
+            #if we've alredy seen the number then continue
             if test == x:
                 continue
             else:
+                #set "test" to the current number
                 test = x
+                #element in nums array is set to "test" value
                 nums[count] = test
+                #count value increments to move onto next index
                 count += 1
         return count
