2dc985c430ee83a53e01c03e420cb92907a56fa6
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2022-08-05 22:31:05 -0500

    Time: 10 ms (99.60%), Space: 13.4 MB (69.71%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -8,6 +8,8 @@ class Solution(object):
             #at the end, convert the whole number to int, +1
             #then convert the whole thing to a string
         a = str(int(s) + 1)
+        #list returns a list object (array delimited by comma) which we
+        #use to split the whole string (number)
         return list(a)
             
         
