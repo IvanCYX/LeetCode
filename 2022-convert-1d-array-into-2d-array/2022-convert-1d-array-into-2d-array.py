01ea68bfe6e9fda149c0f791fe71aa1645c43e61
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2022-08-08 15:28:54 -0500

    Time: 1209 ms (62.04%), Space: 21.5 MB (78.10%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def construct2DArray(self, original, m, n):
+        #check if array can be converted to 2D array
+        if (len(original) != m*n):
+            #if not, return empty array
+            return []
+        else:
+            newArray = []
+            #range(start, stop, step)
+            for i in range (0, len(original), n):
+                newArray.append(original[i:i+n])
+                
+            return newArray
+        
+        
