d8f1fad817a6ce785cdc5feba2aab4698cba2a88
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2022-08-08 15:40:11 -0500

    Time: 9 ms (43.85%), Space: 120.7 MB (22.84%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -1,16 +1,14 @@
-class Solution(object):
-    def construct2DArray(self, original, m, n):
-        #check if array can be converted to 2D array
-        if (len(original) != m*n):
-            #if not, return empty array
-            return []
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
         
-        newArray = []
-        #range(start, stop, step)
-        for i in range (0, len(original), n):
-            if original[i:i+n] != []:
-                newArray.append(original[i:i+n])
-                
-        return newArray
+        int[][] arr = new int[m][n];
         
-        
+        if((m * n) < original.length || (m * n) > original.length) 
+            return new int[0][0];
+        else
+            for(int i = 0; i < m; i++)
+                for(int j = 0; j < n; j++)
+                arr[i][j] = original[(n * i) + j]; 
+       return arr;
+    }
+}
