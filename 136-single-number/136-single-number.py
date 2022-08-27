23fd251553866efef22cb6f29144b51b0f0aaa41
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2022-08-27 17:00:06 -0500

    Time: 217 ms (33.53%), Space: 15.4 MB (90.39%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,9 +1,14 @@
 class Solution(object):
     def singleNumber(self, nums):
         xor = 0
-        
+    
         for i in nums:
             xor ^= i
         
         return xor
         
+        #Xor of any two num gives the difference of bit as 1 and same bit as 0.
+        #Thus, using this we get 1 ^ 1 == 0 because the same numbers have same bits.
+        #So, we will always get the single element because all the same ones will evaluate to 0 and 0^single_number =            single_number.
+        #Time: O(n)
+        #Space: O(1)
